167ccfc9668d02287cf8bde712eda7870552b31d
rtsmc <ryan@trubey.net>
2022-07-03 21:47:24 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+packages used: [pymupdf](https://github.com/pymupdf/PyMuPDF), [Pillow](https://github.com/python-pillow/Pillow)
+
+pymupdf is used for extracting text and images from pdf documents, pillow is used for resizing images
---
+++ main.py
@@ -0,0 +1,113 @@
+import fitz
+import json
+import re
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+from PIL import Image
+
+#get file path of document
+Tk().withdraw()
+file_path = askopenfilename()
+
+#open the document with pymupdf
+document = fitz.Document(file_path)
+
+#extract text from the first page of the document
+text = ""
+for index in range(len(document)):
+    page = document[index]
+    text = text + page.get_text("text", None, 1, None, False)
+output = {}
+
+#function for using regular expression to search the text that was extracted from the document.
+def search_text(pattern: str):
+    match = re.search(pattern, text)
+    if match is None:
+        return ""
+    else:
+        #does some formatting to remove double spaces and any \n characters, also removes leading/trailing whitespace
+        return match.group(2).strip().replace("  ", " ").replace("\n", " ")
+
+#function for extracting and saving an image
+def save_image(page: int, img_number: int, name: str):
+    xref = document.get_page_images(page)[img_number][0]
+    image = document.extract_image(xref)
+    imgout = open(f"output/{name}.{image['ext']}", "wb")
+    imgout.write(image["image"])
+    imgout.close()
+    return image
+
+# If the text includes T-score the pdf is DXA scan result page 1
+if text.find("T-score") != -1:
+    #template file with regular expression strings
+    with open("DXABMD.json") as template:
+        output = json.load(template)
+    template.close()
+
+    save_image(0, 0, "BMDTScoreGraph")
+
+    image = save_image(0, 1, "BMDScan")
+    #Resize BMDScan image because it is distorted
+    img = Image.open(f"output/BMDScan.{image['ext']}")
+    img = img.resize((140, 318))
+    img.save(f"output/BMDScan.{image['ext']}")
+    img.close()
+
+    document.close()
+
+# If the text includes Body Composition then the pdf is DXA scan result page 2
+elif text.find("Body Composition Results") != -1:
+    with open('DXABodyComp.json') as template:
+        output = json.load(template)
+    template.close()
+
+    save_image(0, 0, "TotalBodyPercentFatGraph")
+
+    #Resize BodyComp images because they are distorted
+    image = save_image(0, 1, "BodyCompColor")
+    img = Image.open(f"output/BodyCompColor.{image['ext']}")
+    img = img.resize((140, 318))
+    img.save(f"output/BodyCompColor.{image['ext']}")
+    img.close()
+
+    image = save_image(0, 2, "BodyCompBW")
+    img = Image.open(f"output/BodyCompBW.{image['ext']}")
+    img = img.resize((140, 318))
+    img.save(f"output/BodyCompBW.{image['ext']}")
+    img.close()
+
+    document.close()
+
+elif text.find("DXA Results Summary:") != -1:
+    with open("DXABodyTable.json") as template:
+        output = json.load(template)
+    template.close()
+
+elif text.find("SUCCESS HUB") != -1:
+    with open("Fit3d.json") as template:
+        output = json.load(template)
+    template.close()
+
+    save_image(0, 0, "FrontBody")
+    save_image(0, 1, "SideFrontBody")
+    save_image(0, 2, "BackBody")
+    save_image(0, 3, "SideBody")
+
+    save_image(3, 0, "FrontPosture")
+    save_image(3, 1, "SidePosture")
+
+    document.close()
+
+# If none of the previous if statements were true then the pdf is one that I haven't added code for yet.
+else:
+    print("not a valid pdf file.")
+    exit()
+
+#go through output and change all values from <regular expression string> to search_text(<regular expression string>).
+for i in output:
+    output[i] = search_text(output[i])
+
+#export output to json file.
+with open ("output/output.json", "w") as outfile:
+        json.dump(output, outfile)
+outfile.close()
